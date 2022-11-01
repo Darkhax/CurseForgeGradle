e69849783670a36db93e137b93b426a367454a48
Jonathan Coates <git@squiddev.cc>
2022-11-01 22:53:50 +0000

    Correctly add task dependencies for uploaded files

--- src/main/java/net/darkhax/curseforgegradle/TaskPublishCurseForge.java
+++ src/main/java/net/darkhax/curseforgegradle/TaskPublishCurseForge.java
@@ -1,13 +1,15 @@
 package net.darkhax.curseforgegradle;
 
+import com.google.common.collect.ImmutableList;
 import net.darkhax.curseforgegradle.api.versions.GameVersions;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
 import org.gradle.api.Task;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 
 import javax.annotation.Nullable;
 import java.io.File;
@@ -79,6 +81,11 @@ public class TaskPublishCurseForge extends DefaultTask {
         }
     }
 
+    @Nested
+    public List<UploadArtifact> getUploadArtifacts() {
+        return ImmutableList.copyOf(uploadArtifacts);
+    }
+
     /**
      * Creates a new main level artifact that the plugin will attempt to publish during the {@link #publish()} step.
      * This method requires the minimum amount of information to define an artifact. Further configuration including
@@ -86,14 +93,13 @@ public class TaskPublishCurseForge extends DefaultTask {
      *
      * @param projectId The CurseForge project ID to publish this artifact to.
      * @param toUpload  The artifact to upload when this artifact is published. This can accept files, archive tasks,
-     *                  and several other types of files. The resolution of this is handled by {@link
-     *                  #resolveFile(Object)}.
+     *                  and several other types of files. The resolution of this is handled by {@link FileCollection}.
      * @return An object that represents the artifact being published. This can be used to perform additional
      * configuration such as defining a changelog.
      */
     public UploadArtifact upload(Object projectId, Object toUpload) {
 
-        final UploadArtifact artifact = new UploadArtifact(toUpload, parseLong(projectId), this.log, null);
+        final UploadArtifact artifact = new UploadArtifact(toUpload, parseLong(projectId), getProject().getObjects(), this.log, null);
         this.uploadArtifacts.add(artifact);
         return artifact;
     }
@@ -181,42 +187,16 @@ public class TaskPublishCurseForge extends DefaultTask {
         // and processes the response.
         for (UploadArtifact artifact : this.uploadArtifacts) {
 
-            artifact.prepareForUpload(this.validGameVersions, this::resolveFile);
+            artifact.prepareForUpload(this.validGameVersions);
             artifact.beginUpload(endpointString, tokenString);
 
             // Handle additional files, sometimes called sub files or child files.
             for (UploadArtifact childArtifact : artifact.getAdditionalArtifacts()) {
 
-                childArtifact.prepareForUpload(this.validGameVersions, this::resolveFile);
+                childArtifact.prepareForUpload(this.validGameVersions);
                 childArtifact.beginUpload(endpointString, tokenString);
             }
         }
-    }
-
-    /**
-     * Resolves a file from an object. This supports several ways of defining files such as NIO files, archive tasks, or
-     * standard Gradle project file resolution.
-     *
-     * @param input The input object to resolve.
-     * @return The resolved File reference.
-     */
-    private File resolveFile(Object input) {
-
-        // If the input is a Java file simply cast and return.
-        if (input instanceof File) {
-
-            return (File) input;
-        }
-
-        // If the input is an archive task, request the archive file. This allows stuff like the jar task to be passed
-        // in directly.
-        else if (input instanceof AbstractArchiveTask) {
-
-            return ((AbstractArchiveTask) input).getArchiveFile().get().getAsFile();
-        }
-
-        // Fallback to Gradle's project based file lookup.
-        return this.getProject().file(input);
     }
 
     /**
